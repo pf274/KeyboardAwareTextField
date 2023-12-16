e14966fe2d02cd3c4a4dc2eca697d2aaccfa62e1
pf274 <peterfullmer33@gmail.com>
2023-12-16 10:53:30 -0700

    Bada bing bada boom!

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import KeyboardAvoidingTextInput from './FlexibleTextComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{marginTop: 80}}>Here's an example of keyboard-aware text inputs!</Text>
+      <Image source={require('./assets/icon.png')} style={{width: Dimensions.get("screen").width - 100, height: Dimensions.get("screen").width - 150, marginTop: 20}} />
+      <KeyboardAvoidingTextInput placeholder={"first one"} style={styles.textField} />
+      <KeyboardAvoidingTextInput placeholder={"second one"} style={styles.textField} />
+      <KeyboardAvoidingTextInput placeholder={"third one"} style={styles.textField} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +19,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: 'flex-start',
   },
+  textField: {
+    width: Dimensions.get("screen").width - 100,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    marginTop: 20,
+    paddingLeft: 10,
+    backgroundColor: "white",
+  }
 });
--- FlexibleTextComponent.jsx
+++ FlexibleTextComponent.jsx
@@ -1,7 +1,110 @@
-import { TextInput } from "react-native";
+import { useEffect, useRef } from "react";
+import {
+  TextInput,
+  Keyboard,
+  Dimensions,
+  Animated,
+  useAnimatedValue,
+} from "react-native";
 
-function FlexibleTextComponent({ style }) {
-  return <TextInput placeholder="hello" style={style} />;
+function KeyboardAvoidingTextInput({
+  style,
+  placeholder,
+  onChangeText,
+  onChange,
+  containerStyle = {},
+  value = "",
+}) {
+  const keyboardHeight = useRef(0);
+  const screenHeight = useRef(0);
+  const containerBottom = useRef(0);
+  const containerHeight = useRef(0);
+  const translateY = useAnimatedValue(0);
+  const containerRef = useRef();
+  const textFieldRef = useRef();
+  const initialized = useRef(false);
+
+  function animateUpwards(targetValue, duration = 100) {
+    Animated.timing(translateY, {
+      toValue: targetValue,
+      duration,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  function animateDownwards() {
+    Animated.timing(translateY, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  // get the screen height
+  useEffect(() => {
+    const height = Dimensions.get("screen").height;
+    console.log(`screen height: ${height}`);
+    screenHeight.current = height;
+    Keyboard.addListener("keyboardDidShow", onKeyboardShow);
+    Keyboard.addListener("keyboardDidHide", onKeyboardHide);
+    return () => {
+      Keyboard.removeAllListeners("keyboardDidShow");
+      Keyboard.removeAllListeners("keyboardDidHide");
+    };
+  }, []);
+
+  function calculateComponentBottom(event) {
+    const { height, y } = event.nativeEvent.layout;
+    containerBottom.current = y + height;
+    containerHeight.current = height;
+  }
+
+  function adjustPosition() {
+    console.log(`keyboard height: ${keyboardHeight.current}`);
+    const topOfKeyboard = screenHeight.current - keyboardHeight.current;
+    console.log(`top of keyboard: ${topOfKeyboard}`);
+    console.log(`bottom of component: ${containerBottom.current}`);
+    const adjust =
+      topOfKeyboard - containerBottom.current - containerHeight.current;
+    console.log(`adjust: ${adjust}`);
+    if (adjust < 0) {
+      animateUpwards(adjust);
+    }
+  }
+
+  function onKeyboardShow(event) {
+    keyboardHeight.current = event.endCoordinates.height;
+  }
+  function onKeyboardHide(_) {
+    textFieldRef.current.blur();
+    animateDownwards(500);
+  }
+
+  return (
+    <Animated.View
+      style={[containerStyle, { transform: [{ translateY }] }]}
+      ref={containerRef}
+      onLayout={calculateComponentBottom}
+    >
+      <TextInput
+        placeholder={placeholder}
+        style={style}
+        onFocus={() => {
+          if (!initialized.current) {
+            initialized.current = true;
+            setTimeout(adjustPosition, 250); // leave a little time for the keyboard the show and calculate the height
+            return;
+          }
+          adjustPosition();
+        }}
+        onBlur={animateDownwards}
+        ref={textFieldRef}
+        onChangeText={onChangeText}
+        onChange={onChange}
+        value={onChangeText || onChange ? value : undefined}
+      />
+    </Animated.View>
+  );
 }
 
-export default FlexibleTextComponent;
+export default KeyboardAvoidingTextInput;
